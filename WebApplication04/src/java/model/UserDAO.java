11102ca5f7cbb1bff0a409fe00fa7e982afd4e1e
Nguyễn Quốc Huy <162275710+huynq-1204@users.noreply.github.com>
2025-05-25 10:32:33 +0700

    Add files via upload

---
+++ WebApplication04/src/java/model/UserDAO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class UserDAO {
+
+    ArrayList<UserDTO> list;
+
+    public UserDAO() {
+        this.list = new ArrayList<>();
+        list.add(new UserDTO("admin", "Administrator", "1", "AD", true));
+        list.add(new UserDTO("SE194044", "Nguyen Quoc Huy", "#NQh12042005@", "MB", true));
+    }
+
+    public boolean login(String userID, String password) {
+        for (UserDTO userDTO : list) {
+            if (userDTO.getUserID().equalsIgnoreCase(userID)
+                    && userDTO.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public UserDTO getUserById(String userID) {
+        for (UserDTO userDTO : list) {
+            if (userDTO.getUserID().equalsIgnoreCase(userID)) {
+                return userDTO;
+            }
+        }
+        return null;
+    }
+
+}
