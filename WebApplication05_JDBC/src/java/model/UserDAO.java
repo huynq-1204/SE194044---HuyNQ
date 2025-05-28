537a69e28ef47329c21b29293d12d81ea177de6b
Nguyễn Quốc Huy <162275710+huynq-1204@users.noreply.github.com>
2025-05-28 13:04:16 +0700

    Add files via upload

---
+++ WebApplication05_JDBC/src/java/model/UserDAO.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import utils.DbUtils;
+
+/**
+ *
+ * @author PC
+ */
+public class UserDAO {
+
+    public UserDAO() {
+    }
+
+    public boolean login(String userID, String password) {
+        try {
+            String sql = "SELECT * FROM tblUsers "
+                    + " WHERE userID='" + userID + "' AND password='" + password + "'";
+            //B1: Ket noi
+            Connection conn = DbUtils.getConnection();
+
+            //B2: Tao cong cu thuc thi cau lenh
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            //B3: Duyet bang
+            while (rs.next()) {
+                String userId = rs.getString("userID");
+                String fullName = rs.getString("fullName");
+                String password1 = rs.getString("password");
+                String roleID = rs.getString("roleID");
+                boolean status = rs.getBoolean("status");
+
+                UserDTO userDTO = new UserDTO(userId, fullName, password1, roleID, status);
+                if (userDTO.isStatus()) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public UserDTO getUserById(String userID) {
+        try {
+            String sql = "SELECT * FROM tblUsers "
+                    + " WHERE userID='" + userID + "'";
+            //B1: Ket noi
+            Connection conn = DbUtils.getConnection();
+
+            //B2: Tao cong cu thuc thi cau lenh
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            //B3: Duyet bang
+            while (rs.next()) {
+                String userId = rs.getString("userID");
+                String fullName = rs.getString("fullName");
+                String password1 = rs.getString("password");
+                String roleID = rs.getString("roleID");
+                boolean status = rs.getBoolean("status");
+
+                UserDTO userDTO = new UserDTO(userId, fullName, password1, roleID, status);
+                return userDTO;
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
